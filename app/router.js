bac60f23605a24ae3db725e55c2b676cc5214996
376305 <shishuaiyun@deppon.com>
2021-07-31 18:12:06 +0800

    测试自动化脚本部署

--- app/router.js
+++ app/router.js
@@ -7,10 +7,10 @@ module.exports = app => {
   const { router, controller } = app
   const auth = app.middleware.auth()
 
-  // router.get('/', controller.home.index)
+  router.get('/', controller.home.index)
 
   router.prefix('/api/v1') // 设置基础路径
-
+  
   router.post('/users', controller.user.create) // 用户注册
   router.post('/users/login', controller.user.login) // 用户登录
   router.get('/user', auth, controller.user.getCurrentUser) // 获取当前登录用户信息
